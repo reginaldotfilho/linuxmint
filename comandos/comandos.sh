ee1ddf1a13a2e4a87af2e5731fdee9ebc5c97a8e
Reginaldo Filho <reginaldotf.aluno@gmail.com>
2025-11-17 21:48:26 -0300

    Comandos que usamos no terminal do linux

---
+++ comandos/comandos.sh
@@ -0,0 +1,54 @@
+1  poweroff
+    2  tor
+    3  sudo apt  install tor
+    4  tor
+    5  sudo apt install tor
+    6  sudo apt install torbrowser-launcher
+    7  tor
+    8  sudo apt install curl
+    9  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+   10  sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+   11  sudo apt update
+   12  sudo apt install brave-browser
+   13  sudo pat install notepadqq
+   14  123@senac
+   15  sudo apt install notepadqq
+   16  sudo apt install vim 
+   17  sudo apt update
+   18  sudo apt install git
+   19  sudo inint 0
+   20  sudo init 0
+   21  dsadsa
+   22  sudo apt update
+   23  sudo 
+   24  sudo
+   25  sudo -i
+   26  sudo apt update 
+   27  sudo apt upgrade
+   28  sudo apt dist-upgrade 
+   29  sudi apt full-upgrade
+   30  sudo apt full-upgrade 
+   31  sudo apt autoremove
+   32  sudo apt autoclean
+   33  sudo apt clean
+   34  sudo apt update
+   35  sudo apt install git vim pip python
+   36  sudo apt install git vim  pip python3
+   37  git clone 
+   38  git config --global user.name "
+   39  git config --global user.email 
+   40  git config --global core.editor vim
+   41  git config --global merge.toll vimdiff
+   42  git config --global color.ui true
+   43  git config --list
+   44  git pull
+   45  git status
+   46  git add .
+   47  git commit -m"atualização"
+   48  git commit -m "Atualização"
+   49  git commit -m "atulização"
+   50  git add .
+   51  git commit -m "atualização"
+   52  git status
+   53  git push
+   54  history 
